ad437cdabbcd1c4b761ade3e1a7c535fdff5d6f0
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-09-29 14:39:28 +0200

    WIP19

--- src/internet_identity/src/archive.rs
+++ src/internet_identity/src/archive.rs
@@ -15,7 +15,7 @@ use CanisterInstallMode::Upgrade;
 
 lazy_static! {
     static ref ARCHIVE_HASH: [u8; 32] =
-        hex::decode("3e7af31f4bacf515ebe3c5befa9e7a836f36aa7441a9cbcfdaafa8b7bfd1cbe7")
+        hex::decode("2e15c2b8ada8d154e86439debe31cab4c4bea6c3e4eae1af6b0a2570d1d61309")
             .unwrap()
             .try_into()
             .unwrap();
