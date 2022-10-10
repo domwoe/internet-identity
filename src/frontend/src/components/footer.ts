276148b2f193d271db42c12983facbee8cc2a113
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-10-10 10:33:42 +0200

    wip

--- src/frontend/src/components/footer.ts
+++ src/frontend/src/components/footer.ts
@@ -1,12 +1,35 @@
+
 import { html } from "lit-html";
+import { icBadge } from "./icons";
 
 export const footer = html`<footer class="l-footer">
+<ul class="l-footer__elements">
+    <li class="l-footer__element">
+  <a
+    aria-label="Internet Computer homepage"
+    href="#"
+    rel="noopener noreferrer"
+    target="_blank"
+    >Home</a>
+    </li>
+&#x2022;
+    <li class="l-footer__element">
+  <a
+    aria-label="Internet Computer homepage"
+    href="#"
+    rel="noopener noreferrer"
+    target="_blank"
+    >Help</a>
+    </li>
+&#x2022;
+    <li class="l-footer__element">
   <a
     class="page-signature"
     aria-label="Internet Computer homepage"
     href="https://internetcomputer.org/"
     rel="noopener noreferrer"
     target="_blank"
-    ><img src="/ic-badge.svg" alt="Built on Crypto | Internet Computer"
-  /></a>
+    >${icBadge}</a>
+    </li>
+    </ul>
 </footer>`;
